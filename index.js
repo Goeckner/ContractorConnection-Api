0a96e057f11869ef9acd16ef89feca5491868c22
Justin Goeckner <justingoeckner@gmail.com>
2019-01-16 12:06:35 -0600

    Adding hard coded connection to database

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : "contractorconnectionrds.cj0ol8bj3xlc.us-east-2.rds.amazonaws.com",
+  user     : "Admin",
+  password : "ContractorConnection",
+  port     : "3306"
+});
+connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
+  
+  connection.end();
+
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
