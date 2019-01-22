01e3b9d3fb0a4b046bb1e4e0259d5cfd5b0ff73d
Brady Smith <bradsmi@siue.edu>
2019-01-22 07:17:31 -0600

    Set up a modal function

--- app/model/appModel.js
+++ app/model/appModel.js
@@ -1,1 +1,23 @@
 var sql = require('./db.js')
+
+var User = function(user){
+    this.user = user.user
+    this.status = user.status
+}
+var Instructor = function(inst){
+    this.inst = inst.inst
+    this.status = inst.status
+}
+
+User.createUser = function createUser(newUser, result){
+    sql.query("INSERT INTO users (name, username, password, email, isTrainer) VALUES ?", newUser, function(err, res){
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else{
+            console.log(res);
+            result(null, "success");
+        }
+    })
+}
